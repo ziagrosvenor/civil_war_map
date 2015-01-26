425a878f33e612919929ca43b3d766889ec8c707
ziagrosvenor <ziagrosvenor@live.co.uk>
2015-01-26 13:49:29 +0000

    Change JSON structure for people/belligerents data. Get data for more important people.

--- data/app.js
+++ data/app.js
@@ -16,7 +16,7 @@ var battles = [
   "Battle_of_Turnham_Green"
 ];
 
-var locationsData = [];
+var peopleData = [];
 var battlesData = [];
 
 for(var i = 0; i < battles.length; i++) {
@@ -44,33 +44,63 @@ for(var i = 0; i < battles.length; i++) {
 
     // Battle data
     var outcome = $("tr:last-child th[style] + td").html();
-    var sideOne = $(".infobox tr:nth-child(6) td:first-child a").html();
-    var sideTwo = $(".infobox tr:nth-child(6) td:last-child a").html();
+    var sideOne = $(".infobox tr:nth-child(6) td:first-child a:nth-child(1)").html();
+    var sideTwo = $(".infobox tr:nth-child(6) td:last-child a:nth-child(1)").html();
     var leaderOne = $(".infobox tr:nth-child(8) td:first-child a").html();
-    var leaderTwo = $(".infobox tr:nth-child(8) td:last-child a").html();
+    var leaderTwo = $(".infobox tr:nth-child(8) td:first-child a:last-child").html();
+    var leaderThree = $(".infobox tr:nth-child(8) td:last-child a").html();
+    var leaderFour = $(".infobox tr:nth-child(8) td:last-child a:last-child").html();
 
     // For pages with varying layouts
-    if(sideOne === null && sideTwo === null) {
+    if(sideOne === null) {
       sideOne = $(".infobox tr:nth-child(5) td:first-child a").html();
+    }
+    if(sideTwo === null) {
       sideTwo = $(".infobox tr:nth-child(5) td:last-child a").html();
     }
-    if(leaderOne === null && leaderTwo === null) {
+    if(leaderOne === null) {
       leaderOne = $(".infobox tr:nth-child(7) td:first-child a").html();
+    }
+    if(leaderTwo === null) {
       leaderTwo = $(".infobox tr:nth-child(7) td:last-child a").html();
     }
     if(sideOne.indexOf("img") > 0) {
       console.log('running');
       var sideOne = $(".infobox tr:nth-child(6) td:nth-child(2) a").html();
     }
+    if(leaderOne === leaderTwo || leaderTwo === null || leaderTwo.indexOf("img") > 0 || leaderTwo.indexOf("span") > 0) {
+      sideOneData = {
+        battleName: name,
+        belligerent: sideOne,
+        leader: leaderOne
+      };
+    }
+    else {
+      sideOneData = {
+        battleName: name,
+        belligerent: sideOne,
+        leader: leaderOne,
+        secondLeader: leaderTwo
+      };
+    }
+
+    if(leaderThree === leaderFour) {
+      sideTwoData = {
+        battleName: name,
+        belligerent: sideTwo,
+        leader: leaderThree
+      };
+    }
+    else {
+      sideTwoData = {
+        battleName: name,
+        belligerent: sideTwo,
+        leader: leaderThree,
+        secondLeader: leaderFour
+      }
+    }
 
     // Assign values to an object
-    var locationData = {
-      sideOne: sideOne,
-      sideTwo: sideTwo,
-      leaderOne: leaderOne,
-      leaderTwo: leaderTwo
-    };
-
     var battleData = {
       name: name,
       lat: lat,
@@ -81,15 +111,16 @@ for(var i = 0; i < battles.length; i++) {
     };
 
     // pushes the objects into two arrays
-    locationsData.push(locationData);
+    peopleData.push(sideOneData);
+    peopleData.push(sideTwoData);
     battlesData.push(battleData);
 
     // Saves data as a json file when all battles are scraped
     if(battlesData.length === battles.length) {
       saveFile(battlesData, "civil-war-battles-data");
     }
-    if(locationsData.length === battles.length) {
-      saveFile(locationsData, "civil-war-people-data");
+    if(peopleData.length === (battles.length * 2)) {
+      saveFile(peopleData, "civil-war-people-data");
     }
   });
 }
