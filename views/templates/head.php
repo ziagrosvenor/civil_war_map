d53f8b42b61759ec6a0528ea1a59622816bc0a09
ziagrosvenor <ziagrosvenor@live.co.uk>
2015-02-06 11:57:43 +0000

    Add bootstrap to project.

--- views/templates/head.php
+++ views/templates/head.php
@@ -3,6 +3,7 @@
     <title>Civil War Map</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.css">
     <link rel="stylesheet" href="./assets/css/style.css">
   </head>
   <body>
