ec119a8b86d3c303d4b6936627dc920400086383
ziagrosvenor <ziagrosvenor@live.co.uk>
2015-02-26 15:01:51 +0000

    Update index file. Add RSS.

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <?php
-
-session_start();
-
 require './database/Database.php';
 require './models/battles-model.php';
+require './controllers/PageController.php';
+require './controllers/ResourceController.php';
 
 $Db = Database::getInstance();
 $battles = new Battles($Db);
 
-require './controllers/MapController.php';
-require './controllers/BattlesController.php';
-
-$mapController = new Controller\Map($battles);
-$battlesController = new Controller\Battles($battles);
+$PageController = new Controller\Page($battles);
+$ResourceController = new Controller\Resource($battles);
 
 if(isset($_GET['page'])) {
   $page = $_GET['page'];
+}
+
+if(isset($_GET['id'])) {
+  $battleId = $_GET['id'];
 }
 
 else {
@@ -26,22 +26,45 @@ else {
 if(isset($_GET['json'])) {
   $resource = $_GET['json'];
 
+  header_remove(); 
+  header("Content-Type:application/json");
+
   switch($resource) {
     case 'battles':
-      $battlesController->battlesDataAsJSON();
+      $ResourceController->battlesDataAsJSON();
     break;
   }
 }
 
+// If RSS is requested send data as XML.
+if(isset($_GET['rss'])) {
+  if(isset($battleId)) {
+    header_remove(); 
+    header("Content-Type:text/xml");
+    $ResourceController->renderRSSByBattleId($battleId);
+  }
+  else {
+    echo 'No content found';
+  }
+
+}
+
 // If no JSON resource is requested.
 // serve HTML for application pages or 404 page.
-if(empty($_GET['json'])) {
+if(empty($_GET['json']) && empty($_GET['rss'])) {
+
+  header_remove(); 
+  header("Content-Type:text/html");
+
   switch($page) {
     case 'map':
-      $mapController->mapHomePage();
+      $PageController->mapHomePage();
+    break;
+    case 'battle':
+      $PageController->battlePage($battleId);
     break;
     default:
-      $mapController->pageNotFound();
+      $PageController->pageNotFound();
     die();
   }
 }
